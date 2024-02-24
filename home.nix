1cee4e23c33dd97fd484b9ea32c9e9124e7ae9b6
caio86 <caioluiz86@gmail.com>
2024-02-23 22:07:52 -0300

    update home.nix

--- home.nix
+++ home.nix
@@ -1,40 +1,59 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
-{
+let
+myAliases = {
+  c = "clear";
+  # vim = "nvim";
+  ls = "eza";
+  ll = "eza -lh";
+  lsa = "eza -lah";
+  l = "eza -lah";
+  rg = "rg --smart-case";
+  cat = "bat -p";
+};
+
+in {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "caio";
   home.homeDirectory = "/home/caio";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    home.packages = with pkgs; [
+      eza
+      ripgrep
+      fd
+      bat
+    ];
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-unwrapped;
+    vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      LazyVim
+    ];
+  };
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = myAliases;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "git-auto-fetch" ];
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+  };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -43,7 +62,7 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-
+    
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
@@ -51,25 +70,7 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/caio/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
