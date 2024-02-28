acb59809b4941fb47ef1b1e7ed8063bdcff6a494
caio86 <caioluiz86@gmail.com>
2024-02-27 23:55:18 -0300

    change from gnome to kde

--- configuration.nix
+++ configuration.nix
@@ -49,9 +49,9 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
