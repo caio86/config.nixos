6b2eadcf15da913f9274d9385881c171d4f26fc5
caio86 <caioluiz86@gmail.com>
2024-02-29 15:44:52 -0300

    add git config from github

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, userSettings, ... }:
+{ config, pkgs, lib, userSettings, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -53,6 +53,12 @@
   };
   xdg.mime.enable = true;
   xdg.mimeApps.enable = true;
+  xdg.configFile.nvim.source = pkgs.fetchFromGitHub {
+    owner = "caio86";
+    repo = "config.nvim";
+    rev = "b727bd8dc911d0cf79df12603e0572e6461df6ec";
+    hash = "sha256-+4qowRbs86yvNKxLj2TY9krGI6JN+13E900x+9ukZk8=";
+  };
 
   home.sessionVariables = {
     EDITOR = userSettings.editor;
