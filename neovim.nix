88d8a55987ac1ff6c7e159d19d4fd5421002373f
caio86 <caioluiz86@gmail.com>
2024-03-03 13:06:41 -0300

    change neovim config repository

--- neovim.nix
+++ neovim.nix
@@ -14,4 +14,11 @@
     unzip
     tree-sitter
   ];
+
+  home.file.".config/nvim".source = pkgs.fetchFromGitHub {
+    owner = "caio86";
+    repo = "init.lua";
+    rev = "282d6924ceb101a181f7edb4e46a880313299bc3";
+    hash = "sha256-o+eaZat48OcgDWxMUkZOs9mNB0pO2d72koJycRhkp8w=";
+  };
 }
