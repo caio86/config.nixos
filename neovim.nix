37a5385cf4169f51a0c0709d0bf0c461f761db8c
caio86 <caioluiz86@gmail.com>
2024-03-08 16:56:22 -0300

    update neovim config to latest commit

--- neovim.nix
+++ neovim.nix
@@ -20,7 +20,7 @@
   home.file.".config/nvim".source = pkgs.fetchFromGitHub {
     owner = "caio86";
     repo = "init.lua";
-    rev = "21ea8ba6334f856ea3ea37e0e5cb51f30c324d5d";
-    hash = "sha256-d/Uzdq7bO2CDvbm1WG8pDnFzjE79TIJ6GjvOudesCJc=";
+    rev = "477431222ab92f1cb581753c2cc3cbbe5a4104bd";
+    hash = "sha256-wlLe4kKi8Ka+7xSNV7NwlAhjeVI3mTI5DET0+lrs5i8=";
   };
 }
