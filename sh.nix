6ba942a2af94e31670d528cf1e7bf18c8eeb578e
caio86 <caioluiz86@gmail.com>
2024-03-08 15:17:13 -0300

    update sh.nix
    add bash options
    update aliases
    change zsh theme

--- sh.nix
+++ sh.nix
@@ -3,13 +3,15 @@
 let
   myAliases = {
     c = "clear";
-    # vim = "nvim";
     ls = "eza --icons -hl -T -L=1 --sort=type";
-    ll = "eza -lh";
-    # lsa = "eza -lah";
-    l = "eza -lAh";
+    lsa = "eza --icons -hlA -T -L=1 --sort=type";
+    l = "eza -h --sort=type";
+    la = "eza -hA --sort=type";
+    ll = "eza -lh --sort=type";
     rg = "rg --smart-case";
     cat = "bat -p";
+    gitfetch = "onefetch";
+    lazyvim = "NVIM_APPNAME=lazyvim nvim";
     duh = "sudo du -h --one-file-system --max-depth=1 --threshold=100k";
     dfh = "df -h -x tmpfs -x devtmpfs";
     fzfd = ''
@@ -20,11 +22,13 @@ let
 
       cd $escolhido
     '';
+    nixos-rebuild = "systemd-run --no-ask-password --uid=0 --system --scope -p MemoryLimit=16000M -p CPUQuota=60% nixos-rebuild";
+    home-manager = "systemd-run --no-ask-password --uid=1000 --user --scope -p MemoryLimit=16000M -p CPUQuota=60% home-manager";
   };
 in
 
 {
-  programs.starship.enable = true;
+  programs.starship.enable = false;
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
@@ -33,13 +37,30 @@ in
     shellAliases = myAliases;
     oh-my-zsh = {
       enable = true;
+      theme = "robbyrussell";
       plugins = [
        "git"
        "git-auto-fetch"
-       "command-not-found"
+       # "command-not-found"
        "sudo"
        "aliases"
       ];
     };
   };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    shellAliases = myAliases;
+  };
+
+  home.packages = with pkgs; [
+    disfetch lolcat cowsay onefetch
+    bat eza bottom fd
+    direnv nix-direnv
+  ];
+
+  programs.direnv.enable = true;
+  programs.direnv.enableZshIntegration = true;
+  programs.direnv.nix-direnv.enable = true;
 }
