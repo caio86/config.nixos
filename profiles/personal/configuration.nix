95e5eb5ddaf6ed996eab5bd4c2dcb4c70b79bb52
caio86 <caioluiz86@gmail.com>
2024-03-12 15:46:22 -0300

    change kde plasma from 5 to 6

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -57,7 +57,7 @@
 
   # Enable the KDE Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
