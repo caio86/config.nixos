72adbbe66d05bfe201bb7becc0c8c153edf94f78
caio86 <caioluiz86@gmail.com>
2024-03-12 15:46:07 -0300

    fix import link

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ../../system/hardware-configuration.nix
     ];
 
   # Experimental features
