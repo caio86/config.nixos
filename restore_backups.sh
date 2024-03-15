6be03c2e5c83294cc6fc8d2cb8069f4a94007715
caio86 <caioluiz86@gmail.com>
2024-03-15 10:30:02 -0300

    update backup script

--- restore_backups.sh
+++ restore_backups.sh
@@ -2,17 +2,21 @@
 
 input="./backups"
 
-set -x
+# set -x
 
 pattern="'(.*?)'"
 
-while IFS= read -r line; do
-  # mv $HOME/$line.backups $HOME/$line
-  if [[ $line =~ $pattern ]]; then
-    echo "sim"
-  else
-    echo "não"
-  fi
-  # echo "mv $HOME/$line.backups $HOME/$line"
-done < $input
+echo $(awk -F "'" '/Existing/ {print $2}' < $input)
+
+# while IFS= read -r line; do
+#   # mv $HOME/$line.backups $HOME/$line
+#   result= $(echo $line | awk -F "'" '/Existing/ {print $2}')
+#   if [[ $result == " " ]]; then
+#     echo "tem"
+#   else
+#     echo "Não tem"
+#   fi
+#   # echo "Output: {$result}"
+#   # echo "mv $HOME/$line.backups $HOME/$line"
+# done < $input
 
