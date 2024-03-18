006518216d21988b52f2a0536bc0ab35c68fcd8a
caio86 <caioluiz86@gmail.com>
2024-03-18 12:52:26 -0300

    fix wrong username in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sed -i "0,/teste/s//$(whoami)/" ./flake.nix
-sed -i "0,/teste/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" ./flake.nix
+sed -i "0,/caiol/s//$(whoami)/" ./flake.nix
+sed -i "0,/Caio Luiz/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" ./flake.nix
 
 # Open editor to manually edit flake.nix
 if [ -z "$EDITOR" ]; then
