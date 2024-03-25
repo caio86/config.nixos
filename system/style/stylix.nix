53cdfd9df347996f55db3eb32ef37d18ae65bf2e
caio86 <caioluiz86@gmail.com>
2024-03-25 15:12:04 -0300

    add base16 colorscheme from catppuccin

--- system/style/stylix.nix
+++ system/style/stylix.nix
@@ -1,7 +1,18 @@
 { pkgs, stylix, userSettings, ... }:
 
+
+let
+  themePath = "../../catppuccin-mocha.yaml";
+  backgroundImage = pkgs.fetchurl {
+    url = "https://w.wallhaven.cc/full/m3/wallhaven-m3gxg8.png";
+    hash = "sha256-aSgwSZpIRKMdCNYbbpyY3GCb8C8B71SwsH6NYWVA41k=";
+  };
+in
 {
   imports = [ stylix.nixosModules.stylix ];
 
-  stylix.image = ~/wallpapers/doom.jpg;
+  stylix.autoEnable = true;
+  stylix.polarity = "dark";
+  stylix.image = backgroundImage;
+  stylix.base16Scheme = ./. + themePath;
 }
