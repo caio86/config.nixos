490120c7535de3199c5aad9dcec6ec2ef5121cd2
caio86 <caioluiz86@gmail.com>
2024-03-27 19:48:46 -0300

    edit stylix config

--- system/style/stylix.nix
+++ system/style/stylix.nix
@@ -2,7 +2,7 @@
 
 
 let
-  themePath = "../../catppuccin-mocha.yaml";
+  themePath = "../../../catppuccin-mocha.yaml";
   backgroundImage = pkgs.fetchurl {
     url = "https://w.wallhaven.cc/full/m3/wallhaven-m3gxg8.png";
     hash = "sha256-aSgwSZpIRKMdCNYbbpyY3GCb8C8B71SwsH6NYWVA41k=";
@@ -11,8 +11,12 @@ in
 {
   imports = [ stylix.nixosModules.stylix ];
 
-  stylix.autoEnable = true;
+  stylix.autoEnable = false;
   stylix.polarity = "dark";
   stylix.image = backgroundImage;
   stylix.base16Scheme = ./. + themePath;
+
+  stylix.targets.grub.enable = true;
+
+  stylix.targets.console.enable = true;
 }
