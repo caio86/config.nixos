8a29c9a29ac6454c1f119254e02bd6bdae487a33
caio86 <caioluiz86@gmail.com>
2024-03-27 19:48:52 -0300

    disabled cava app

--- user/shell/cli-apps.nix
+++ user/shell/cli-apps.nix
@@ -5,12 +5,19 @@
   home.packages = with pkgs; [
     neofetch # System info
     starfetch # Random stars info
-    lolcat cowsay
-    cava # Visualize audio in terminal
+    lolcat
+    cowsay
+    # cava # Visualize audio in terminal
     killall # kill process by name
-    bat eza fd bottom ripgrep
-    tree tldr
-    zip unzip
+    bat
+    eza
+    fd
+    bottom
+    ripgrep
+    tree
+    tldr
+    zip
+    unzip
     ranger # file manager
     w3m # Text pased web browser
     vim
