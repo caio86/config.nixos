62201c444a4c448e5f9ec1cb1677312602d9ab30
caio86 <caioluiz86@gmail.com>
2024-05-14 21:04:31 -0300

    modify sshd to only allow connection with a ssh key

--- system/security/sshd.nix
+++ system/security/sshd.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ userSettings, authorizedKeys ? [ ], ... }:
 
 {
   services.openssh = {
     enable = true;
     openFirewall = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
   };
+  users.users.${userSettings.username}.openssh.authorizedKeys.keys = authorizedKeys;
 }
