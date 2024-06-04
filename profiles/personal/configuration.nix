e2dbb93fd3669078a36024d5bbb2befa83ac05d3
caio86 <caioluiz86@gmail.com>
2024-06-04 00:48:32 -0300

    fix NIX-PATH nixos-config path

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -24,7 +24,7 @@
   # Fix nix path
   nix.nixPath = [
     "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
-    "nixos-config=$HOME/.dotfiles/system/configuration.nix"
+    "nixos-config=$HOME/.dotfiles/system/hardware-configuration.nix"
     "/nix/var/nix/profiles/per-user/root/channels"
   ];
 
