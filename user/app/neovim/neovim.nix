845789cf8a9fc1668f8a9b817c81bebb6b1ea6a4
caio86 <caioluiz86@gmail.com>
2024-06-17 21:07:19 -0300

    update neovim

--- user/app/neovim/neovim.nix
+++ user/app/neovim/neovim.nix
@@ -21,7 +21,7 @@
   home.file.".config/nvim".source = pkgs.fetchFromGitHub {
     owner = "caio86";
     repo = "init.lua";
-    rev = "e5592ef32d75cd647b7aa7de28ae8b2dbde75847";
-    hash = "sha256-pNXeP0/hyhdmiWBTGhIKjZcIiRNiFEgAsozuSCldl0w=";
+    rev = "b68908ccd18d872be7daac248a9e85bdce0e2b15";
+    hash = "sha256-asttjL1BwW/li7JVKAvmd36QvA06Ph9mfBFWSaJogCY=";
   };
 }
