5ee7779628adb47a1683ded16b4cb03839535735
caio86 <caioluiz86@gmail.com>
2024-08-12 12:29:16 -0300

    remove unused fonts

--- system/wm/fonts.nix
+++ system/wm/fonts.nix
@@ -5,15 +5,7 @@
   fonts.packages = with pkgs; [
     # Fonts
     (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Inconsolata" ]; })
-    powerline
-    inconsolata
-    inconsolata-nerdfont
-    iosevka
     font-awesome
-    ubuntu_font_family
-    terminus_font
-    fira-code
-    fira-code-symbols
   ];
 }
 
