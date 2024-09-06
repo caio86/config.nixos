12b90046946ac0c8af39b9b5a18116e724b2bd7a
caio86 <caioluiz86@gmail.com>
2024-09-06 09:31:05 -0300

    create i3wm file

---
+++ system/wm/i3wm.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./pipewire.nix
+    ./dbus.nix
+    ./fonts.nix
+    ./gnome-keyring.nix
+    ./xserver.nix
+  ];
+
+  services.xserver = {
+      displayManager = {
+        defaultSession = "none+i3";
+      };
+
+      windowManager.i3 = {
+        enable = true;
+        extraPackages = with pkgs; [
+          dmenu
+          i3status
+          i3lock
+          i3blocks
+        ];
+      };
+  };
+}
